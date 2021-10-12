de974a8cdc976cf84dab4ae2a4bfeb28d9c45faa
donah <dona.hsieh@gmail.com>
2021-10-12 11:55:49 -0700

    Add functions to product list details and create page. Add observables to service.

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-details',
@@ -7,9 +9,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  currentProduct: any;
+  message = '';
+
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.message = '';
+    this.getProduct(this.route.snapshot.paramMap.get('id'));
   }
 
+  getProduct(id: any): void {
+    this.productService.read(id)
+      .subscribe(
+        product => {
+          this.currentProduct = product;
+          console.log(product);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  setAvailableStatus(status: boolean): void {
+    const data = {
+      name: this.currentProduct.name,
+      description: this.currentProduct.description,
+      available: status
+    };
+
+    this.productService.update(this.currentProduct.id, data)
+      .subscribe(
+        response => {
+          this.currentProduct.available = status;
+          console.log(response);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  updateProduct(): void {
+    this.productService.update(this.currentProduct.id, this.currentProduct)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.message = 'The product was updated!';
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  deleteProduct(): void {
+    this.productService.delete(this.currentProduct.id)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.router.navigate(['/products']);
+        },
+        error => {
+          console.log(error);
+        });
+  }
 }
--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -7,9 +8,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  products: any;
+  currentProduct: null | undefined = null;
+  currentIndex = -1;
+  name = '';
+
+  constructor( private productService: ProductService ) { }
 
   ngOnInit(): void {
+    this.readProducts();
   }
 
+  readProducts(): void {
+    this.productService.readAll()
+      .subscribe(
+        products => {
+          this.products = products;
+          console.log(products);
+        },
+        error => {
+          console.log(error);
+        }
+      );
+  }
+
+  refresh(): void {
+    this.readProducts();
+    this.currentProduct = null;
+    this.currentIndex = -1;
+  }
+
+  setCurrentProduct(product: null | undefined, index: number): void {
+    this.currentProduct = product;
+    this.currentIndex = index;
+  }
+
+  deleteAllProducts(): void {
+    this.productService.deleteAll()
+      .subscribe(
+        response => {
+          console.log(response);
+          this.readProducts();
+        },
+        error => {
+          console.log(error);
+        }
+      )
+  }
+
+  searchByName(): void {
+    this.productService.searchByName(this.name)
+      .subscribe(
+        products => {
+          this.products = products;
+          console.log(products);
+        },
+        error => {
+          console.log(error);
+        }
+      )
+  }
 }
